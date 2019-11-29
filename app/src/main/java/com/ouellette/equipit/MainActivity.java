7eaa0d2be5f1526f14531f0a5b931bedef46c5af
ariouellette2000 <43147334+ariouellette2000@users.noreply.github.com>
2019-11-29 17:54:52 -0500

    Shake animation added

--- app/src/main/java/com/ouellette/equipit/MainActivity.java
+++ app/src/main/java/com/ouellette/equipit/MainActivity.java
@@ -3,6 +3,7 @@ package com.ouellette.equipit;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.view.Menu;
@@ -16,15 +17,16 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.widget.Toolbar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import com.ouellette.equipit.data.DatabaseHandler;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity {
 
     private Toolbar myToolBar;
     private Button startButton;
 
     private String selectedColor;
-
 
 
     @Override
@@ -33,8 +35,8 @@ public class MainActivity extends AppCompatActivity{
 
         //Color theme
 //        //Reach prefs
-        SharedPreferences mPref = getSharedPreferences("idval",0);
-        String selectColor = mPref.getString("color_editor","");
+        SharedPreferences mPref = getSharedPreferences("idval", 0);
+        String selectColor = mPref.getString("color_editor", "");
 
         //change color when click
         switch (selectColor) {
@@ -63,16 +65,17 @@ public class MainActivity extends AppCompatActivity{
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent (MainActivity.this,Category.class));
+                startActivity(new Intent(MainActivity.this, Category.class));
+                shakeAnimation();
             }
         });
 
         //Message Toast when item added
         String intentAdd = getIntent().getStringExtra("add_product_name");
-        if(intentAdd != null){
+        if (intentAdd != null) {
             Context context = getApplicationContext();
-            String toasttext = "You've added "+intentAdd;
-            Toast.makeText(context, toasttext,Toast.LENGTH_SHORT).show();
+            String toasttext = "You've added " + intentAdd;
+            Toast.makeText(context, toasttext, Toast.LENGTH_SHORT).show();
         }
 
     }
@@ -87,7 +90,7 @@ public class MainActivity extends AppCompatActivity{
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.options_menu,menu);
+        inflater.inflate(R.menu.options_menu, menu);
 
         return true;
     }
@@ -95,15 +98,15 @@ public class MainActivity extends AppCompatActivity{
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch(item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.menuitem_settings:
-                startActivity(new Intent (MainActivity.this,Settings.class));
+                startActivity(new Intent(MainActivity.this, Settings.class));
                 return true;
             case R.id.menuitem_add_text:
-                startActivity(new Intent (MainActivity.this,Add.class));
+                startActivity(new Intent(MainActivity.this, Add.class));
                 return true;
             case R.id.menuitem_home:
-                startActivity(new Intent (MainActivity.this,MainActivity.class));
+                startActivity(new Intent(MainActivity.this, MainActivity.class));
                 return true;
             default:
                 //not recognize so invoke superclass
@@ -111,4 +114,23 @@ public class MainActivity extends AppCompatActivity{
         }
     }
 
+    private void shakeAnimation() {
+        Animation shake = AnimationUtils.loadAnimation(MainActivity.this, R.anim.shake_animation);
+        startButton.startAnimation(shake);
+
+        shake.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+    }
 }
